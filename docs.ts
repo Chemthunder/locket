8d87dd999ea7ecae1e331006132af427b8970997
chemthunder <chemthunderisme@gmail.com>
2026-03-19 10:37:39 -0400

    somewhat finalized question mark

--- docs.ts
+++ docs.ts
@@ -1,7 +1,25 @@
-
+/**
+ * Locket provides various utilities for the Backend.
+ */
+//% advanced=true
+//% weight=90 color=#ffa95e icon="\uf06d"
 namespace locket {
 
 
+    /**
+     * Creates a new Menu Entry within the base MakeCode menu.
+     * @param name The name of the Menu Entry
+     * @param handler The functionality of the Menu Entry
+     * @param icon The Menu Entry's icon.
+     */
+    //% block="create Menu Entry with $name, $handler, $icon"
+    //% blockId=locketmenuentry
+    //% name.defl=menuName
+    //% handler.defl=null
+    //% icon.defl=myIcon
+    //% weight=97
+    //% blockSetVariable=myMenuEntry
+    //% expandableArgumentMode=toggle
     export function createMenuEntry(name: string, handler: () => void, icon: Image): MenuEntry {
         function nameToFunction(): string {
             return name.toUpperCase();
